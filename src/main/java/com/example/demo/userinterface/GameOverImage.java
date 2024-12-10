368883908bdc17f065df3fc22b5ed8207fa71be2
Mohammed Ahnaf Habib <ahnafhabib2002@gmail.com>
2024-12-11 06:05:24 +0800

    Changed the game over image

--- src/main/java/com/example/demo/userinterface/GameOverImage.java
+++ src/main/java/com/example/demo/userinterface/GameOverImage.java
@@ -5,11 +5,12 @@ import javafx.scene.image.ImageView;
 
 public class GameOverImage extends ImageView {
 	
-	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
+	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.jpg";
 
 	public GameOverImage(double xPosition, double yPosition) {
 		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
+		setFitWidth(1300);
+		setFitHeight(750);
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
 	}
